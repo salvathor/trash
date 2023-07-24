af42ce02025a3e18c436f9002754492866b0661c
Salvador Lopez <salvatore.isc@gmail.com>
2023-07-24 07:56:18 -0600

    Initial Commit

---
+++ trash/trashApp.swift
@@ -0,0 +1,17 @@
+//
+//  trashApp.swift
+//  trash
+//
+//  Created by Salvador Lopez on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct trashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
